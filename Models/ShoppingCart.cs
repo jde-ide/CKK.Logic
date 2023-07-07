830477aaf9e437424670c044d0362f2eed15e812
Jackson <jdeatchel@gmail.com>
2023-07-06 20:04:40 -0600

    Add Shopping Cart class

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class ShoppingCart
+    {
+        private Customer customer;
+        private ShoppingCartItem product1;
+        private ShoppingCartItem product2;
+        private ShoppingCartItem product3;
+
+        public ShoppingCart(Customer customer)
+        {
+            this.customer = customer;
+        }
+
+        public int GetCustomerId()
+        {
+            return customer.GetId();
+        }
+
+        public ShoppingCartItem AddProduct(Product prod, int quantity)
+        {
+            if (quantity > 0)
+            {
+                if (/* checking if product exists */)
+                {
+                    // add quantity if found
+                }
+                else
+                {
+                    // add product if doesn't exist
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
